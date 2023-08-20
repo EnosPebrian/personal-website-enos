e64bb84e32f400e5570d0c328be0967344b9c949
EnosPebrian <enospebrian1@gmail.com>
2023-08-20 20:35:08 +0700

    edit href navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,18 @@ export const TopNavbar = () => {
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">Enos Pebrian</Navbar.Brand>
+          <Navbar.Brand href="#cover-container">Enos Pebrian</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">About</Nav.Link>
-              <Nav.Link href="#link">Works</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <Nav.Link
+                target="_blank"
+                href="https://enospebrian.github.io/HTML-Try-Out/public/contact.html"
+              >
+                About
+              </Nav.Link>
+              <Nav.Link href="#portfolio">Works</Nav.Link>
+              <NavDropdown title="More" id="basic-nav-dropdown" disabled>
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
                   Another action
